0e9a31f018a97b02fe895d2fa1120c5e7e2c0afa
metalheadcode <metalheadcoder@gmail.com>
2021-08-28 22:26:53 +0800

    revamp header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,20 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import SpeacialHeader from "./Header/SpeacialHeader"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <>
+    <BgHeader />
+    <RowHeader>
+      <Logo>
+        <Link to="/">{siteTitle}</Link>
+      </Logo>
+      <Menu>M</Menu>
+    </RowHeader>
+    <SpeacialHeader />
+  </>
 )
 
 Header.propTypes = {
@@ -40,3 +26,39 @@ Header.defaultProps = {
 }
 
 export default Header
+
+const BgHeader = styled.div`
+  position: absolute;
+  left: 0;
+  background-color: black;
+  width: 100vw;
+  height: 56px;
+  z-index: -1;
+`
+
+const RowHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Logo = styled.div`
+  padding: 20px;
+  a {
+    color: white;
+    font-weight: 600;
+  }
+`
+const Menu = styled.div`
+  padding: 20px;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 3000ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    color: red;
+    box-shadow: 5px 6px 36px 2px rgba(255, 0, 0, 0.2);
+    -webkit-box-shadow: 5px 6px 36px 2px rgba(255, 0, 0, 0.2);
+    -moz-box-shadow: 5px 6px 36px 2px rgba(255, 0, 0, 0.2);
+  }
+`
