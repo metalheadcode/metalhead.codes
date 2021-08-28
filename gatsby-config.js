95ee8249b5f79c8daf65ace785f4351d36ce12da
metalheadcode <metalheadcoder@gmail.com>
2021-08-28 22:34:02 +0800

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/satanism.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
