f3d9be0646e98b017395c105de71ac5b402b0b83
metalheadcode <metalheadcoder@gmail.com>
2021-08-31 14:50:55 +0800

    adding parallaxone in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Seo from "../components/seo"
 import Layout from "../components/layout"
 import HeroesOne from "../components/Heroes/HeroesOne"
 import HeroesTwo from "../components/Heroes/HeroesTwo"
+import ParallexOne from "../components/Parallex/ParallexOne"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <ParallexOne />
     <HeroesOne />
     <HeroesTwo />
   </Layout>
