633c59d2d7551a3422ce21f7dc3dc2eabae3319c
metalheadcode <metalheadcoder@gmail.com>
2021-09-18 01:06:30 +0800

    install gatsby datocms

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,33 @@ module.exports = {
     siteUrl: `https://metalhead.codes`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: `ebcc4a2db97119f5f72dd991755610`,
+
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+
+        // Custom API base URL (most don't need this)
+        // apiUrl: 'https://site-api.datocms.com',
+
+        // Setup locale fallbacks
+        // In this example, if some field value is missing in Italian, fall back to English
+        localeFallbacks: {
+          it: ["en"],
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
