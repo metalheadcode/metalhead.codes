7813adbc12770ef70b61c916ad9e82b168b7db06
metalheadcode <metalheadcoder@gmail.com>
2021-09-18 01:07:40 +0800

    create collectionscard component:

---
+++ src/components/Cards/CollectionsCard.js
@@ -0,0 +1,86 @@
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import React from "react"
+import styled from "styled-components"
+
+const CollectionsCard = ({ data, bg, color }) => {
+  console.log("Collecitons Data", data)
+  const image = getImage(data.websiteScreenshot)
+  return (
+    <CaseCardItem color={color}>
+      <CardTitle>{data.originalId}</CardTitle>
+      <Frame>
+        <GatsbyImage image={image} />
+      </Frame>
+      <Divider color={color} />
+      <Item>
+        <SubItemLeft>Company</SubItemLeft>
+        <SubItemRight>{data.company}</SubItemRight>
+      </Item>
+      <Divider color={color} />
+      <Item>
+        <SubItemLeft>Web</SubItemLeft>
+        <SubItemRight>
+          <a href={data.link} target="_blank" rel="noreferrer">
+            {data.link}
+          </a>
+        </SubItemRight>
+      </Item>
+      <Divider color={color} />
+      <p>Note</p>
+      <Description>{data.note}</Description>
+    </CaseCardItem>
+  )
+}
+
+export default CollectionsCard
+
+const CaseCardItem = styled.div`
+  color: ${props => props.color};
+  display: grid;
+  padding: 10px;
+  gap: 10px;
+  border-radius: 7px;
+
+  a {
+    color: ${props => props.color};
+  }
+`
+
+const Frame = styled.div`
+  border: 10px solid white;
+  border-radius: 3px;
+`
+
+const Divider = styled.div`
+  height: 0.5px;
+  background-color: ${props => props.color};
+  opacity: 0.15;
+`
+
+const Item = styled.div`
+  display: flex;
+`
+const SubItemLeft = styled.p`
+  flex: 1;
+  text-overflow: ellipsis;
+`
+const SubItemRight = styled.p`
+  flex: 3;
+  width: 120px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const CardTitle = styled.h1`
+  font-weight: 300;
+  font-size: 64px;
+`
+const Description = styled.p`
+  line-height: 130%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* number of lines to show */
+  -webkit-box-orient: vertical;
+`
