fc009a34ed3ff24240345992ce3cd7d676cf3b08
metalheadcode <metalheadcoder@gmail.com>
2021-09-18 01:08:08 +0800

    edit case studies

--- src/components/Sections/CaseStudies.js
+++ src/components/Sections/CaseStudies.js
@@ -2,137 +2,20 @@ import React from "react"
 import styled from "styled-components"
 import CaseCard from "../Cards/CaseCard"
 
-const dummyData = [
-  {
-    id: 1,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.facebook.com",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 2,
-    designer: "Nor Ummairah",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 3,
-    designer: "Ahmad Syazwan",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 4,
-    designer: "Anas Nazmi",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 5,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 6,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 7,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 8,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 9,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 10,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 11,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 12,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 13,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 14,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-  {
-    id: 15,
-    designer: "Ahmad Hazim",
-    year: 2021,
-    location: "German",
-    link: "www.sdasdasdad.asdas",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit fugiat magni quibusdam tempora quo et doloribus culpa iusto ex ipsum reprehenderit perferendis repellendus dolores odio debitis dolorem soluta corporis sequi hic, suscipit corrupti. Consequatur esse aliquam reprehenderit nobis pariatur adipisci, harum tenetur molestias saepe fugiat voluptatum nulla ex temporibus.",
-  },
-]
-
-const CaseStudies = () => {
+const CaseStudies = ({ caseStudies }) => {
+  const caseStudiesData = caseStudies.nodes
   return (
     <Section>
       <Wrapper>
         <BigTitle>Case Studies</BigTitle>
         <CardGrid>
-          {dummyData.map(data => (
-            <CaseCard key={data.id} data={data} />
+          {caseStudiesData.map(data => (
+            <CaseCard
+              key={data.originalId}
+              data={data}
+              color="#EBE4E4"
+              bg="#0d1821"
+            />
           ))}
         </CardGrid>
       </Wrapper>
@@ -143,18 +26,19 @@ const CaseStudies = () => {
 export default CaseStudies
 
 const Section = styled.section`
-  background: black;
+  background: #0d1821;
+  color: #ebe4e4;
   padding: 15px;
   min-height: 100vh;
 `
 const Wrapper = styled.div``
 
 const BigTitle = styled.h1`
-  color: white;
   font-size: 110pt;
   font-weight: 700;
   padding-top: 64px;
   padding-bottom: 64px;
+  word-break: break-all;
 
   @media (max-width: 500px) {
     font-size: 64pt;
