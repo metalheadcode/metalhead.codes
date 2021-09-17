f9588b0170b344c0b73c95d7a908157655173b43
metalheadcode <metalheadcoder@gmail.com>
2021-09-18 01:08:23 +0800

    create query from graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,14 @@ import Layout from "../components/layout"
 // import HeroesTwo from "../components/Heroes/HeroesTwo"
 // import ParallexOne from "../components/Parallex/ParallexOne"
 import CaseStudies from "../components/Sections/CaseStudies"
+import Collections from "../components/Sections/Collections"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <CaseStudies />
+    <Collections collections={data.collections} />
+    <CaseStudies caseStudies={data.caseStudies} />
     {/* <ParallexOne />
     <HeroesOne />
     <HeroesTwo /> */}
@@ -17,3 +20,29 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    caseStudies: allDatoCmsCaseStudy {
+      nodes {
+        originalId
+        designer
+        date
+        link
+        description
+      }
+    }
+    collections: allDatoCmsCollection {
+      nodes {
+        company
+        link
+        note
+        originalId
+        websiteScreenshot {
+          originalId
+          gatsbyImageData(width: 700, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
