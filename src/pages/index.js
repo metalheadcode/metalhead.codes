91b310793fcb74e110e41ac732f7586418dceb28
metalheadcode <metalheadcoder@gmail.com>
2021-09-19 23:20:38 +0800

    add resource inside and remove unnecessary component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,15 @@ import Layout from "../components/layout"
 // import ParallexOne from "../components/Parallex/ParallexOne"
 import CaseStudies from "../components/Sections/CaseStudies"
 import Collections from "../components/Sections/Collections"
+import Resources from "../components/Sections/Resources"
 import { graphql } from "gatsby"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <Collections collections={data.collections} />
+    <Resources resources={data.resources} />
     <CaseStudies caseStudies={data.caseStudies} />
-    {/* <ParallexOne />
-    <HeroesOne />
-    <HeroesTwo /> */}
   </Layout>
 )
 
@@ -40,6 +39,16 @@ export const query = graphql`
         originalId
         websiteScreenshot {
           originalId
+          gatsbyImageData(width: 700, placeholder: BLURRED)
+        }
+      }
+    }
+    resources: allDatoCmsResource {
+      nodes {
+        originalId
+        link
+        description
+        image {
           gatsbyImageData(width: 700, placeholder: BLURRED)
         }
       }
