bb4d111c973bf5ef1bc6ab393c66f38939abf07d
metalheadcode <metalheadcoder@gmail.com>
2021-09-28 08:51:52 +0800

    add swiper pagination style

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 import "swiper/css"
-// import "swiper/css/pagination"
+import "swiper/css/pagination"
 import "swiper/css/navigation"
 import { ThemeProvider } from "../context/theme.context"
 
